71dc27bef334abf97d7b693e326ffd807cbd48a6
IchmitZ <egruemer@gmail.com>
2021-12-28 22:30:16 +0100

    Finished Layout of GUI

--- core/form.py
+++ core/form.py
@@ -1,10 +1,39 @@
 #REPRESENT THE INPUT IN PYTHON DATA TYPES BEFORE WRITING TO FILES
 # @FINN TODO: convert input from GUI to an instance of this class
 
-
 class Form:
     def __init__(self):
-        pass
-    
-    def get_input():
-        age = str(age_entry.get())
+        self.age: int = None
+        self.sex: str = None
+        self.culture: str = None
+        self.luck = {
+            "color": None,
+            "reason": None
+        }
+        self.color = {
+            "clauset": None,
+            "noble": None,
+            "high-quality": None,
+            "car": None,
+            "learning": None,
+            "relaxing": None,
+        }
+        self.association_red: str = None
+        self.room = {
+            "color": None,
+            "conscious decision": None,
+            "reason": None,
+            "ambience": None,
+        }
+        self.meaning_red: str = None  # war ? brutality
+        self.flag = [[True, None], False] #flag[0][1] represents meaning of flag given by participant
+        self.check_ingredients: int = None
+        self.music_genre = {
+            "schlager": None,
+            "pop": None,
+            "classic": None,
+            "rap": None,
+            "electro": None
+        }
+        self.favourite_color: str = None
+        self.fits_personality: bool = None
---
+++ poll.py
@@ -0,0 +1,3 @@
+from shell import gui
+
+gui.run()
