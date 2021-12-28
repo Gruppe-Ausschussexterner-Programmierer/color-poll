7afe9066c5ff55d93d8a9470582767d969082e37
ERROR Emil Not Found <fyoug8gle@gmail.com>
2021-12-28 20:20:38 +0100

    created form class

--- core/form.py
+++ core/form.py
@@ -4,7 +4,37 @@
 
 class Form:
     def __init__(self):
-        pass
-    
-    def get_input():
-        age = str(age_entry.get())
+        self.age = None
+        self.sex = None
+        self.culture = None
+        self.luck = {
+            "color": None,
+            "reason": None
+        }
+        self.color = {
+            "clauset": None,
+            "noble": None,
+            "high-quality": None,
+            "car": None,
+            "learning": None,
+            "relaxing": None,
+        }
+        self.association_red = None
+        self.room = {
+            "color": None,
+            "conscious decision": None,
+            "reason": None,
+            "ambience": None,
+        }
+        self.meaning_red = None  # war ? brutality
+        self.flag = [[True, None], False] #flag[0][1] represents meaning of flag given by participant
+        self.check_ingredients: int = None
+        self.music_genre = {
+            "schlager": None,
+            "pop": None,
+            "classic": None,
+            "rap": None,
+            "electro": None
+        }
+        self.favourite_color = None
+        self.fits_personality: bool = None
---
+++ core/form_manager.py
@@ -0,0 +1,7 @@
+from form import Form
+
+if __name__ == "__main__":
+    form = Form()
+
+    form.age = "lol"
+    print(form.age)
