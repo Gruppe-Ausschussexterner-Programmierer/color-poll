e8f55e2acc29cba3003d47a4925091add2061ed0
IchmitZ <91048342+IchmitZ-1@users.noreply.github.com>
2021-12-28 12:15:56 +0100

    Update gui.py

--- face/gui.py
+++ face/gui.py
@@ -6,7 +6,7 @@ from core import form
 
 #setup root
 root = tk.Tk()
-root.geometry("760x540")
+root.geometry("750x700")
 root.title("Auswertungsbogen")
 root.resizable(0, 0)
 
@@ -20,7 +20,7 @@ root.columnconfigure(3, weight=3)"""
 age_label = ttk.Label(root, text="Alter:")
 age_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
 
-age_entry = tk.Entry(root)
+age_entry = ttk.Entry(root)
 age_entry.grid(row=0, column=1, padx=5, pady=5)
 
 #sex
@@ -69,76 +69,81 @@ luck_entry_reason = ttk.Entry(root)
 luck_entry_reason.grid(row=3, column=5, padx=5, pady=5)
 
 #closet color
-#noble colors
-#high-quality colors
-#car color
-#association red
-#room color
-#room atmosphere
-#learning color
-#relaxing color
-#word association red
-#flag association
-#sweets ingredients
-#"Schlager" color
-#pop color
-#classic color
-#rap color
-#electro color
-#favorite color
+closet_label = ttk.Label(root, text="Häufigste Farbe im Kleiderschrank:")
+closet_label.grid(row=4, column=0, padx=5, pady=5, sticky=tk.W)
 
-#submit button @Emil G. (command=get_input (#form_function))
+#noble colors
+closet_label = ttk.Label(root, text="Signalfarbe Edel:")
+closet_label.grid(row=5, column=0, padx=5, pady=5, sticky=tk.W)
+
+#high-quality colors
+closet_label = ttk.Label(root, text="Signalfarbe Hochwertig:")
+closet_label.grid(row=6, column=0, padx=5, pady=5, sticky=tk.W)
+
+#car color
+closet_label = ttk.Label(root, text="Farbe Traumauto:")
+closet_label.grid(row=7, column=0, padx=5, pady=5, sticky=tk.W)
+
+#association red
+closet_label = ttk.Label(root, text="Assoziation Rot:")
+closet_label.grid(row=8, column=0, padx=5, pady=5, sticky=tk.W)
+
+#room color
+closet_label = ttk.Label(root, text="Zimmer Farbe")
+closet_label.grid(row=9, column=0, padx=5, pady=5, sticky=tk.W)
+
+#room atmosphere
+closet_label = ttk.Label(root, text="Zimmer Atmosphäre:")
+closet_label.grid(row=10, column=0, padx=5, pady=5, sticky=tk.W)
+
+#learning color
+closet_label = ttk.Label(root, text="Farbe zum Lernen:")
+closet_label.grid(row=11, column=0, padx=5, pady=5, sticky=tk.W)
+
+#relaxing color
+closet_label = ttk.Label(root, text="Farbe zum Entspannen:")
+closet_label.grid(row=12, column=0, padx=5, pady=5, sticky=tk.W)
+
+#word association red
+closet_label = ttk.Label(root, text="Word-Assoziation Rot:" )
+closet_label.grid(row=13, column=0, padx=5, pady=5, sticky=tk.W)
+
+#flag association
+closet_label = ttk.Label(root, text="Assoziation Flagge:")
+closet_label.grid(row=14, column=0, padx=5, pady=5, sticky=tk.W)
+
+#sweets ingredients
+closet_label = ttk.Label(root, text="Zutaten in Süßkram:")
+closet_label.grid(row=15, column=0, padx=5, pady=5, sticky=tk.W)
+
+#"Schlager" color
+closet_label = ttk.Label(root, text="Schlager Farbe:")
+closet_label.grid(row=16, column=0, padx=5, pady=5, sticky=tk.W)
+
+#pop color
+closet_label = ttk.Label(root, text="Pop Farbe:")
+closet_label.grid(row=17, column=0, padx=5, pady=5, sticky=tk.W)
+
+#classic color
+closet_label = ttk.Label(root, text="Klassik Farbe:")
+closet_label.grid(row=18, column=0, padx=5, pady=5, sticky=tk.W)
+
+#rap color
+closet_label = ttk.Label(root, text="Rap Farbe:")
+closet_label.grid(row=19, column=0, padx=5, pady=5, sticky=tk.W)
+
+#electro color
+closet_label = ttk.Label(root, text="Elekto Farbe:")
+closet_label.grid(row=20, column=0, padx=5, pady=5, sticky=tk.W)
+
+#favorite color
+closet_label = ttk.Label(root, text="Lieblingsfarbe:")
+closet_label.grid(row=21, column=0, padx=5, pady=5, sticky=tk.W)
+
+#submit
+submit_button = ttk.Button(root, text="Abschicken")
+submit_button.grid(row=22, column=5, padx=5, pady=5, sticky=tk.E)
 
 
 
 root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""Label(root, text="Alter").grid(row=0, sticky=W)
-Label(root, text="Geschlecht").grid(row=1, sticky=W)
-Label(root, text="1. Welche Farbe signalisiert Glück?").grid(row=2, sticky=W)
-Label(root, text="2. Erklärung").grid(row=3, sticky=W)
-Label(root, text="3.").grid(row=4, sticky=W)
-Label(root, text="4.").grid(row=5, sticky=W)
-Label(root, text="5.").grid(row=6, sticky=W)
-Label(root, text="6.").grid(row=7, sticky=W)
-Label(root, text="7.").grid(row=8, sticky=W)
-Label(root, text="8.").grid(row=9, sticky=W)
-Label(root, text="9.").grid(row=10, sticky=W)
-Label(root, text="10.").grid(row=11, sticky=W)
-Label(root, text="11.").grid(row=12, sticky=W)
-Label(root, text="12.").grid(row=13, sticky=W)
-Label(root, text="13.").grid(row=14, sticky=W)
-Label(root, text="14.").grid(row=15, sticky=W)
-Label(root, text="15.").grid(row=16, sticky=W)
-Label(root, text="16.").grid(row=17, sticky=W)
-Label(root, text="17.").grid(row=18, sticky=W)
-Label(root, text="18.").grid(row=19, sticky=W)
-
-sex_button_male = Button(root, text="Männlich")
-sex_button_female = Button(root, text="Weiblich")
-sex_button_queer = Button(root, text="Anderes Geschlecht")
-culture_button = Button(root, text="Deutsch")
-culture_input = Entry(root)
-favorite_color_red = Button(root, text="     ",bg="red", fg="white")
-
-
-age_input.grid(row=0, column=1)
-sex_button_male.grid(row=1, column=1)
-sex_button_female.grid(row=1, column=2)
-sex_button_queer.grid(row=1, column=3)
-culture_button.grid(row=2, column=1)
-culture_input.grid(row=2, column=2)
-favorite_color_red.grid(row=3, column=1)"""
