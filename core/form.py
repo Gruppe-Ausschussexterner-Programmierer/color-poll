ce6a70eb73f60eb84e7a1a8f6474f13604f6fbe7
IchmitZ <egruemer@gmail.com>
2021-12-30 18:05:57 +0100

    Visually finished GUI, added "Middle-End" between gui.py and form.py, deleted appearence.py

--- core/form.py
+++ core/form.py
@@ -1,11 +1,11 @@
 #REPRESENT THE INPUT IN PYTHON DATA TYPES BEFORE WRITING TO FILES
 # @FINN TODO: convert input from GUI to an instance of this class
 
-from shell.gui import entry_age, checkbutton_sex, entry_culture, luck_color, entry_luck_reason, closet_color
+from shell.gui import *
 
 class Form:
     def __init__(self):
-        self.age : int = entry_age #TODO: convert to int (is str rn)
+        self.age : int = entry_age # @SCHLÄGER TODO: convert to int (is str rn)
         self.sex : int = checkbutton_sex # 0->None, 1->Male, 2->Female, 3->Other
         self.culture : str = entry_culture
         self.luck = {
@@ -14,28 +14,28 @@ class Form:
         }
         self.color = {
             "clauset": closet_color,
-            "noble": None,
-            "high-quality": None,
-            "car": None,
-            "learning": None,
-            "relaxing": None,
+            "noble": noble_color_1, # @SCHLÄGER TODO: add a second noble color
+            "high-quality": quality_2, # @SCHLÄGER TODO: add a second high-quality color
+            "car": car,
+            "learning": learning_color,
+            "relaxing": relaxing_color,
         }
-        self.association_red : str = None
+        self.association_red : str = red_entry
         self.room = {
-            "color": None,
-            "conscious decision": None,
-            "reason": None,
-            "ambience": None,
+            "color": room_color,
+            "conscious decision": checkbutton_room_color_deliberatly_chosen, # 0->None, 1->Yes, 2->No
+            "reason": entry_room_color_reason,
+            "ambience": entry_room_atmosphere,
         }
-        self.meaning_red : str = None  # war ? brutality
-        self.flag_meaning : str = None #leave None when participant didn't know
-        self.check_ingredients : int = None
+        self.meaning_red : int = checkbutton_red # 0->None, 1->Love, 2->War
+        self.flag_meaning : int = checkbutton_flag # 0->None, 1->Yes, 2->No
+        self.check_ingredients : int = sweet_ingredients # @SCHLÄGER TODO: convert to int (is str rn)
         self.music_genre = {
-            "schlager": None,
-            "pop": None,
-            "classic": None,
-            "rap": None,
-            "electro": None
+            "schlager": schlager_color,
+            "pop": pop_color,
+            "classic": classic_color,
+            "rap": rap_color,
+            "electro": electro_color
         }
-        self.favourite_color : str = None
-        self.fits_personality : bool = None
+        self.favourite_color : str = favorite_color
+        self.fits_personality : int = checkbutton_favorite_color # 0->None, 1->Yes, 2->No
